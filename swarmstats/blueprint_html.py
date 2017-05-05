e11a1ae27774b5b4ea6b6a729c06b6fc813ec3be
Rob Kooper <kooper@illinois.edu>
2017-05-05 17:03:40 -0500

    documentation

--- swarmstats/blueprint_html.py
+++ swarmstats/blueprint_html.py
@@ -10,16 +10,25 @@ blueprint = flask.Blueprint('html',  __name__, static_folder='static', template_
 @blueprint.route('/<page>')
 @utils.requires_user("admin", "viewer")
 def html_page(page):
+    """
+    Return any static page with .html appended.
+    """
     return blueprint.send_static_file('%s.html' % page)
 
 
 @blueprint.route('/services/<service>/logs')
 @utils.requires_user("admin", "viewer")
 def html_services_logs(service):
+    """
+    Return log viewer for a specific service.
+    """
     (k, v) = utils.find_item(swarm.instance.services, service)
     return flask.render_template('logs.html', service_id=k, service_name=v['name'], containers=v['containers'])
 
 
 @blueprint.route('/swagger')
 def html_swagger():
+    """
+    Return swagger documentation
+    """
     return blueprint.send_static_file('swagger.yml')
